a1676161679a3d3c8c5986a82504be24c0b69d2d
Rafael Dalsenter <rafaeldalsenter@gmail.com>
2020-06-11 18:23:37 -0300

    Testes unitários dos services

--- tests/AzureFunctionGithub.Services.Tests/CsprojFileServicesTests.cs
+++ tests/AzureFunctionGithub.Services.Tests/CsprojFileServicesTests.cs
@@ -1,7 +1,60 @@
-﻿namespace AzureFunctionGithub.Services.Tests
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace AzureFunctionGithub.Services.Tests
 {
     public class CsprojFileServicesTests
     {
+        private readonly ICsprojFileServices _csprojFileServices;
         
+        public CsprojFileServicesTests()
+        {
+            _csprojFileServices = new CsprojFileServices();    
+        }
+        
+        [Fact]
+        public void GetChangedReferences_WithMultipleReferencesChanged()
+        {
+            var path = "";
+            var resultExpected = new string[3]{ "System.Data", "System.Generics", "Sys.Hrg"};
+
+            var result = _csprojFileServices.GetChangedReferences(path);
+            
+            Assert.Collection(result, x => Assert.Equal(resultExpected[0], x),
+                x => Assert.Equal(resultExpected[1], x),
+                x => Assert.Equal(resultExpected[2], x));
+        }
+
+        [Fact]
+        public void GetChangedReferences_WithOneReferenceChanged()
+        {
+            var path = "";
+            var resultExpected = "System.Data";
+
+            var result = _csprojFileServices.GetChangedReferences(path);
+            
+            Assert.Collection(result, x => Assert.Equal(resultExpected, x));
+        }
+
+        [Fact]
+        public void GetChangedReferences_WithoutReferenceChanged()
+        {
+            var path = "";
+
+            var result = _csprojFileServices.GetChangedReferences(path);
+            
+            Assert.Empty(result);
+        }
+
+        [Fact]
+        public void GetChangedReferences_WithoutChanged()
+        {
+            var path = "";
+
+            var result = _csprojFileServices.GetChangedReferences(path);
+            
+            Assert.Empty(result);
+        }
     }
 }
--- tests/AzureFunctionGithub.Services.Tests/PullRequestInfoServicesTests.cs
+++ tests/AzureFunctionGithub.Services.Tests/PullRequestInfoServicesTests.cs
@@ -1,7 +1,26 @@
-﻿namespace AzureFunctionGithub.Services.Tests
+﻿using System.Threading.Tasks;
+using Xunit;
+
+namespace AzureFunctionGithub.Services.Tests
 {
     public class PullRequestInfoServicesTests
     {
-        
+        [Fact]
+        public async Task Get_WithSomeFileModified()
+        {
+            
+        }
+
+        [Fact]
+        public async Task Get_WithSomeFileModifiedNoneCsproj()
+        {
+            
+        }
+
+        [Fact]
+        public async Task Get_WithoutFilesModified()
+        {
+            
+        }
     }
 }
